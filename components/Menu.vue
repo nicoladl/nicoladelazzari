56c3265d1c469b4db68845710dcc443ecc0777f0
Nicola De Lazzari <nicola.delazzari@gmail.com>
2019-05-08 22:23:05 +0200

    add new menu

---
+++ components/Menu.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="menu">
+    <ul>
+      <li class="menu__item">ciao</li>
+      <li class="menu__item">ciao</li>
+      <li class="menu__item">ciao</li>
+      <li class="menu__item">ciao</li>
+      <li class="menu__item">ciao</li>
+    </ul>
+  </nav>
+</template>
