0b45cd32ec691275b060bae83374464ce1f8a78a
Nicola De Lazzari <nicola.delazzari@gmail.com>
2019-05-08 22:22:56 +0200

    add sidebar on layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    <nuxt />
+  <div class="row">
+    <div class="col-xs-2">
+      <Menu />
+    </div>
+    <div class="col-xs-10">
+      <nuxt />
+    </div>
   </div>
 </template>
+
+<script>
+import Menu from '~/components/Menu'
+
+export default {
+  components: {
+    Menu
+  }
+}
+</script>
