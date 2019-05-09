32ccd8dbd0153119800f3857ecb33ee1fbe41f71
Nicola De Lazzari <nicola.delazzari@gmail.com>
2019-05-09 21:04:16 +0200

    add fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,8 @@ export default {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: "stylesheet", href: 'MyFontsWebfontsKit.css' }
     ]
   },
 
