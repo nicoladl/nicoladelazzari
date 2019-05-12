29277bc1da051df060d3ad391e7ea7ac283ac725
Nicola De Lazzari <nicola.delazzari@gmail.com>
2019-05-12 14:34:57 +0200

    fix typo

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
   <section class="section">
-    <p class="footer">This website was design by Giovanni Pante</p>
+    <p class="footer">This website was design by Giovanni Panté</p>
   </section>
 </template>
