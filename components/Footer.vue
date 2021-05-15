5cf7a01f769a7908d8fea7da355cddd68663732f
Nicola <nicola.delazzari@gmail.com>
2021-05-16 01:43:06 +0200

    fix footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="section section--bottomHalf">
-    <p class="footer">
+    <p class="footer text--light">
       This website was design by
-      <a href="http://www.giovannipante.com" target="_blank">Giovanni Pante</a>
+      <a href="http://www.giovannipante.com" target="_blank" class="link">Giovanni Pante</a>
     </p>
   </section>
 </template>
