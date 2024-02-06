7e9b044aa4e3659aaa9b4f3c8365ff9f67940154
Nicola De Lazzari <nicola.delazzari@gmail.com>
2024-02-06 09:13:48 +0100

    refactor: remove section title

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,16 +1,9 @@
 <template>
-  <section class="section" data-id="5">
-    <ul>
-      <li class="row">
-        <div class="col-xs-3 no-padding-right nomobile">
-          <div class="text text--right text--light reveal">
-            <span class="item">Social</span>
-          </div>
-        </div>
-        <div class="col-xs-12 col-md-9">
-          <div v-for="(contact, index) in contacts" :key="index" class="social">
-            <Link :item="contact" />
-          </div>
+  <section class="section row end-xs" data-id="5">
+    <ul class="list col-xs-12 col-md-9">
+      <li>
+        <div v-for="(contact, index) in contacts" :key="index" class="social">
+          <Link :item="contact" />
         </div>
       </li>
     </ul>
