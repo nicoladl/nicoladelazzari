c475ca97da8e15d07f2e0e8c1c112970dd376a98
nicoladl <nicoladl@users.noreply.github.com>
2026-01-09 19:47:29 +0000

    deploy: 952b1f5cafd855acc249c261a810bd4deb976fe9

--- sw.js
+++ sw.js
@@ -6,20 +6,20 @@ workbox.precaching.precacheAndRoute([
     "revision": "4fb9e5d141f194479841e5722cddab2e"
   },
   {
-    "url": "/_nuxt/0f36434.js",
+    "url": "/_nuxt/6c8891d.js",
+    "revision": "5f6178e56270c3442b9cbd50993e7b5c"
+  },
+  {
+    "url": "/_nuxt/6cd6da3.js",
+    "revision": "584fa2c55e4cc4146b71ec7d48889e00"
+  },
+  {
+    "url": "/_nuxt/b18a5b3.js",
     "revision": "dd9209cb2fcd61b00fefc2e50fb9f4ec"
   },
   {
-    "url": "/_nuxt/2d29a64.js",
+    "url": "/_nuxt/f1783a8.js",
     "revision": "2cf3fcb377ef3469dc476df58d0ed7a5"
-  },
-  {
-    "url": "/_nuxt/30e5add.js",
-    "revision": "17ecbdc94fd9d042f4f60b40dd4df17e"
-  },
-  {
-    "url": "/_nuxt/6c8891d.js",
-    "revision": "5f6178e56270c3442b9cbd50993e7b5c"
   }
 ], {
   "cacheId": "nicoladelazzari",
